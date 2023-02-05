bff3fb89629be5980eee8e9f1ded0b10446697e2
kjeon0901 <kjeon0901@naver.com>
2023-02-05 18:13:09 +0900

    add - B5_놀람_BOJ10926.java

---
+++ Implementation/B5_놀람_BOJ10926.java
@@ -0,0 +1,10 @@
+package Implementation;
+import java.util.Scanner;
+
+public class B5_놀람_BOJ10926 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.nextLine();
+        System.out.println(id+"??!");
+    }
+}
